45c8f6623ee80214f3cb5f124e5543fad100e5bd
BarminaSK <barminask@mail.ru>
2025-11-18 15:54:56 +0500

    Task4

---
+++ Tyuiu.BarminaSK.Sprint5.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Tyuiu.BarminaSK.Sprint5.Task4.V10.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint5.Task4.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask4V10.txt";
+            double result = ds.LoadFromDataFile(path);
+
+            double x = 2.74;
+            double expected = Math.Pow(x, 3) * 1.2 * x + 2;
+            expected = Math.Round(expected, 3);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.BarminaSK.Sprint5.Task4.V10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Tyuiu.BarminaSK.Sprint5.Task4.V10.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint5.Task4.V10.Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Бармина С.К. | ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнила: Бармина София Константиновна | ПИНб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask4V10.txt";
+            Console.WriteLine($"Файл: {path}");
+            Console.WriteLine($"Формула: x^3*1.2*х + 2");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.LoadFromDataFile(path);
+            Console.WriteLine($"Результат: {result}");
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
