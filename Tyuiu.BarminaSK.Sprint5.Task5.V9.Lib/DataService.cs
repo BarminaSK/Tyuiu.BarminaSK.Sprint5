4c3f822530359551288c19f861d49b471e94b786
BarminaSK <barminask@mail.ru>
2025-11-18 16:32:19 +0500

    Task5

---
+++ Tyuiu.BarminaSK.Sprint5.Task5.V9.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.BarminaSK.Sprint5.Task5.V9.Lib
+{
+    public class DataService : ISprint5Task5V9
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string fileContent = File.ReadAllText(path);
+
+            string[] numbers = fileContent.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int maxInt = numbers
+                .Select(numStr =>
+                {
+                    if (double.TryParse(numStr, NumberStyles.Any, CultureInfo.InvariantCulture, out double result))
+                    {
+                        return result;
+                    }
+                    return double.MinValue;
+                })
+                .Where(num => num == Math.Floor(num))
+                .Select(num => (int)num) 
+                .Max();
+
+            return maxInt;
+        }
+    }
+}
---
+++ Tyuiu.BarminaSK.Sprint5.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Tyuiu.BarminaSK.Sprint5.Task5.V9.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint5.Task5.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V9.txt";
+            double result = ds.LoadFromDataFile(path);
+            double expected = 17;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
